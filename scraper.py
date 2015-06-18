be7056a2bcaf9657d6f0e52c95e6af9c37c6286e
gratimax <max@ovsankin.com>
2015-06-17 17:47:54 -0700

    Start work on parsing class info
    The way that class information is structured is absolutely horrid.
    Classes are stored as table rows, with each meeting a new table row.
    The next step is to write a special processor that checks if each
    table row is a class definition and acts accordingly.

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 from pyquery import PyQuery as pq
+from collections import namedtuple
 import json
 import requests
 
 CLASSES_SCHEDULE = 'https://www.deanza.edu/schedule/classes/index.html'
 CLASSES_SEARCH = 'https://www.deanza.edu/schedule/classes/schsearch.html'
 
-def get_departments_list():
+def get_departments():
   print "Downloading departments list..."
   search_page = pq(requests.get(CLASSES_SCHEDULE).text)
   department_options = search_page('#Uniq_Course_ID').items('option')
@@ -24,27 +25,27 @@ def department_classes_request(department_id):
   }
 
   payload = {
-    'QuarterQtr': 'M',
-    'QuarterYear': '2015',
-    'sortBy': '1',
     'Quarter': '2015M',
     'Uniq_Course_ID': department_id,
-    'CourseID': '',
-    'CourseTitle': '',
-    'Instructor': '',
-    'Location': ''
   }
 
   return requests.post(CLASSES_SEARCH, headers=headers, data=payload)
 
-def get_classes_for_department(department_id, department_name):
+ClassSection = namedtuple('ClassSection', ['crn', 'course', 'title', 'meetings'])
+ClassMeeting = namedtuple('ClassMeeting', ['time', 'days', 'instructor', 'location', 'type'])
+
+def get_classes(department_id, department_name):
   print "Downloading classes for department %s" % department_name
   classes_page = pq(department_classes_request(department_id).text)
-  print classes_page('.snews').text()
+  classes_table = classes_page('.anti_nav_print_adj').eq(2)
+  classes_rows = list(classes_table.items('tr'))[3:]
+  print [elem.text() for elem in classes_rows]
   #print [elem('.snews') for elem in classes_page("anti_nav_print_adj").items("tr")]
 
-departments = get_departments_list()
+departments = get_departments()
 
 print "Downloading class information..."
-for department_id in departments:
-  get_classes_for_department(department_id, departments[department_id])
+# for department_id in departments:
+#   get_classes(department_id, departments[department_id])
+department_id = 'MATH'
+get_classes(department_id, departments[department_id])
